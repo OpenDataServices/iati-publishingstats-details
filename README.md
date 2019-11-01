89a4d6adea6b1fc945dba9411e2b53c6230a9786
Ben Webb <ben.webb@opendataservices.coop>
2019-11-01 13:18:36 +0000

    Add values for end dates and budget years to CSV

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ Note: This repo requires Python 2.7, because they IATI-Publishing-Statistics cod
 ```
 git clone git@github.com:OpenDataServices/iati-publishingstats-details.git
 cd iati-publishingstats-details
+# then put some IATI activities files into data/subdir
 git submodule init
 git submodule update
 virtualenv .ve
 source .ve/bin/activate
 pip install -r requirements.txt
 ./fetch_helpers.sh
-python forward_looking_details.py
+python forward_looking_details.py > forward_looking_details.csv
 ```
--- forward_looking_details.py
+++ forward_looking_details.py
@@ -14,6 +14,7 @@ from stats.common import budget_year
 from stats.dashboard import ActivityStats
 
 writer = csv.writer(sys.stdout)
+writer.writerow(["iati-identifier", "First year to fail", "End dates", "Budget years"])
 
 for fpath in glob.glob("data/*/*"):
     for activity in etree.parse(fpath).xpath("/iati-activities/iati-activity"):
@@ -26,16 +27,24 @@ for fpath in glob.glob("data/*/*"):
                 activity_stats._planned_end_code(), activity_stats._actual_end_code()
             )
         )
+        budgets = activity.xpath("./budget")
 
         iati_identifier = activity.xpath("./iati-identifier/text()")[0]
-        reason_current = ""
-        reason_budget = ""
-        if not end_dates:
-            reason_current = "No end dates"
-        if not activity.xpath("./budget"):
-            reason_budget = "No budgets"
+        end_dates_str = ""
+        budget_years_str = ""
+        if end_dates:
+            end_dates_str = ",".join(x.xpath("@iso-date")[0] for x in end_dates)
+        else:
+            end_dates_str = "No end dates"
+        if budgets:
+            budget_years = [budget_year(x) for x in budgets]
+            budget_years_str = ",".join(str(x) for x in budget_years)
+        else:
+            budget_years_str = "No budgets"
 
         for year in sorted(current.keys()):
             if current[year] and not w_budgets[year]:
-                writer.writerow([iati_identifier, year, reason_current, reason_budget])
+                writer.writerow(
+                    [iati_identifier, year, end_dates_str, budget_years_str]
+                )
                 break
